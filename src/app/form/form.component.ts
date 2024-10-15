dd7e34659a846401bc9180c0485e89251f3a0821
Antoine Germon <antoine.germon@etu.unistra.fr>
2024-10-15 21:27:05 +0200

    TP2 fini

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from "@angular/forms";
+/* ngIf */
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
@@ -21,16 +23,74 @@ export class FormComponent {
     password : String = "";
     confirmPassword : String = "";
     login : String = "";
+    showRecap : boolean = false;
+
+    ngOnInit() {
+        const inputs = document.querySelectorAll('input');
     
-  onSubmit() {
-    this.valid = !this.valid;
-  }
+        inputs.forEach(input => {
+            input.addEventListener('blur', (event) => {
+                const target = event.target as HTMLInputElement;
+                const errorMessage = document.getElementById(`error-${target.name}`);
+                if (!target.checkValidity()) {
+                    target.classList.add('invalid');
+                    if (errorMessage) {
+                        errorMessage.style.display = 'block';
+                    }
+                } else {
+                    target.classList.remove('invalid');
+                    if (errorMessage) {
+                        errorMessage.style.display = 'none';
+                    }
+                }
+            });
+        });
+    } 
+    
+    onSubmit(event: Event) {
+        event.preventDefault();
+        const inputs = document.querySelectorAll('input');
+        let formValid = true;
 
-  onMouseEnter() {
-    document.querySelector('.paolo-img')?.classList.remove('hidden');
-  }
+        inputs.forEach(input => {
+            if (!input.checkValidity()) {
+                formValid = false;
+                input.classList.add('invalid');
+                const errorMessage = document.getElementById(`error-${input.name}`);
+                if (errorMessage) {
+                    errorMessage.style.display = 'block';
+                }
+            }
+        });
 
-  onMouseLeave() {
-    document.querySelector('.paolo-img')?.classList.add('hidden');
-  }
+        let password = document.getElementById('password') as HTMLInputElement;
+        let confirmPassword = document.getElementById('confirmPassword') as HTMLInputElement;
+
+        if (password.value !== confirmPassword.value) {
+            formValid = false;
+            password.classList.add('invalid');
+            confirmPassword.classList.add('invalid');
+            const errorMessage = document.getElementById(`error-${confirmPassword.name}`);
+            if (errorMessage) {
+                errorMessage.innerHTML = 'Les mots de passe ne correspondent pas';
+                errorMessage.style.display = 'block';
+            }
+        }
+
+        if (formValid) {
+            this.showRecap = true;
+        } else {
+            this.showRecap = false;
+        }
+        console.log('formValid', formValid);
+        console.log('this', this);
+    }
+
+    onMouseEnter() {
+        document.querySelector('.paolo-img')?.classList.remove('hidden');
+    }
+
+    onMouseLeave() {
+        document.querySelector('.paolo-img')?.classList.add('hidden');
+    }
 }
